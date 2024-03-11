8af005e6ab34912e3e7357b64d02462643ace3f9
Ana <analuizadiasdarocha212@gmail.com>
2024-03-11 16:41:46 -0300

    Correção Read e Delete

--- front/src/pages/categories.jsx
+++ front/src/pages/categories.jsx
@@ -1,39 +1,80 @@
+import { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Table from "react-bootstrap/Table";
 
-
 const Categories = () => {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    updateTable();
+  });
+  
+  const readCategoria = async () => {
+    const response = await fetch("http://localhost/routes/categories.php");
+    const data = await response.json();
+    return data;
+  };
+
+  const updateTable = async () => {
+    const dbCategoria = await readCategoria();
+    setCategories(dbCategoria);
+  };
+  updateTable();
+
+  const deleteCategoria = async (id) => {
+    await fetch(`http://localhost/routes/categories.php?id=${id}`, {
+      method: "DELETE",
+    }),
+      window.location.reload();
+  };
+
   return (
     <>
       <div className="conteiner col-12">
         <div className="col-5">
-          <Form id="form">
+          <Form>
             <Form.Group className="mb-3">
-              <Form.Control type="text" placeholder="Category name" />
+              <Form.Control type="text" placeholder="Category Name" />
             </Form.Group>
 
             <Form.Group className="mb-3">
               <Form.Control type="number" min="1" placeholder="Tax" />
             </Form.Group>
 
-            <Button variant="light" className="mt-4 purple" type="submit">
-              Add Category
+            <Button variant="light" type="submit" className="mt-4 purple">
+              Add category
             </Button>
           </Form>
         </div>
 
         <div className="col-5">
-          <Table responsive="sm" id="tbCategoria">
+          <Table responsive="sm">
             <thead>
               <tr>
                 <th>Code</th>
-                <th>Category Name</th>
+                <th>Category</th>
                 <th>Tax</th>
                 <th></th>
               </tr>
             </thead>
-            <tbody></tbody>
+            <tbody>
+              {categories.map((category) => (
+                <tr>
+                  <td>{category.code}</td>
+                  <td>{category.name}</td>
+                  <td>{category.tax}%</td>
+                  <td>
+                    <button
+                      type="button"
+                      onClick={() => deleteCategoria(category.code)}
+                      className="gbt ml"
+                    >
+                      delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
           </Table>
         </div>
       </div>
@@ -41,37 +82,70 @@ const Categories = () => {
   );
 };
 
-
-const readCategoria = async () => {
-  const response = await fetch("http://localhost/routes/categories.php");
-  const data = await response.json();
-  return data;
-};
-const clearTable = () => {
-  const rows = document.querySelectorAll("#tbCategoria>tbody tr");
-  rows.forEach((row) => row.parentNode.removeChild(row));
-};
-const updateTable = async () => {
-  clearTable();
-  const dbCategoria = await readCategoria();
-  dbCategoria.forEach((category, index) => {
-    createRow(category, index);
-  });
-};
-updateTable();
-
-const createRow = (category) => {
-  const newRow = document.createElement("tr");
-  newRow.innerHTML = `
-    <td>00${category.code}</td>
-    <td>${category.name}</td>
-    <td>${category.tax}%</td>
-    <td class="tdbutton">
-        <button type="button" onclick="deleteCategoria(${category.code})">Delete</button>
-    </td>
-    
-  `;
-  document.querySelector("#tbCategoria>tbody").appendChild(newRow);
-};
-
 export default Categories;
+
+// import Button from "react-bootstrap/Button";
+// import Form from "react-bootstrap/Form";
+// import Table from "react-bootstrap/Table";
+
+// const categorys = () => {
+//   return (
+//     <>
+//       <div className="conteiner col-12">
+//         <div className="col-5">
+
+//         </div>
+//         <div className="col-5">
+//           <Table responsive="sm" id="tbProduto">
+//             <thead>
+//               <tr>
+//                 <th>Code</th>
+//                 <th>category</th>
+//                 <th>Amount</th>
+//                 <th>Unit Price</th>
+//                 <th>categorys</th>
+//                 <th></th>
+//               </tr>
+//             </thead>
+//             <tbody></tbody>
+//           </Table>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// const readCategoria = async () => {
+//   const response = await fetch("http://localhost/routes/categorys.php");
+//   const data = await response.json();
+//   return data;
+// };
+// const clearTable = () => {
+//   const rows = document.querySelectorAll("#tbProduto>tbody tr");
+//   rows.forEach((row) => row.parentNode.removeChild(row));
+// };
+// const updateTable = async () => {
+//   clearTable();
+//   const dbCategoria = await readCategoria();
+//   dbCategoria.forEach((category, index) => {
+//     createRow(category, index);
+//   });
+// };
+// updateTable();
+// const createRow = (category, index) => {
+//   const newRow = document.createElement("tr");
+//   newRow.innerHTML = `
+//       <td>00${category.code}</td>
+//       <td>${category.name}</td>
+//       <td>${category.amount}</td>
+//       <td>$${category.price}</td>
+//       <td>${category.categorys}</td>
+//       <td class="tdbutton">
+//         <button type="button" onclick="deleteCategoria(${category.code})">Delete</button>
+//       </td>
+
+//     `;
+//   document.querySelector("#tbProduto>tbody").appendChild(newRow);
+// };
+
+// export default categorys;
--- front/src/pages/products.jsx
+++ front/src/pages/products.jsx
@@ -1,8 +1,33 @@
+import { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Table from "react-bootstrap/Table";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    updateTable();
+  });
+  
+  const readProduto = async () => {
+    const response = await fetch("http://localhost/routes/products.php");
+    const data = await response.json();
+    return data;
+  };
+
+  const updateTable = async () => {
+    const dbProduto = await readProduto();
+    setProducts(dbProduto);
+  };
+  updateTable();
+
+  const deleteProduto = async (id) => {
+    await fetch(`http://localhost/routes/products.php?id=${id}`, {
+      method: "DELETE",
+    }),
+      window.location.reload();
+  };
+
   return (
     <>
       <div className="conteiner col-12">
@@ -20,19 +45,14 @@ const Products = () => {
               <Form.Control type="number" min="1" placeholder="Price" />
             </Form.Group>
 
-            <Form.Select className="mb-3 select">
-              <option>Category</option>
-              <option>Default select</option>
-              <option>Default select</option>
-            </Form.Select>
-
             <Button variant="light" type="submit" className="mt-4 purple">
               Add Product
             </Button>
           </Form>
         </div>
+
         <div className="col-5">
-          <Table responsive="sm" id="tbProduto">
+          <Table responsive="sm">
             <thead>
               <tr>
                 <th>Code</th>
@@ -43,44 +63,97 @@ const Products = () => {
                 <th></th>
               </tr>
             </thead>
-            <tbody></tbody>
+            <tbody>
+              {products.map((product) => (
+                <tr>
+                  <td>{product.code}</td>
+                  <td>{product.name}</td>
+                  <td>{product.amount}</td>
+                  <td>${product.price}</td>
+                  <td>{product.category}</td>
+                  <td>
+                    <button
+                      type="button"
+                      onClick={() => deleteProduto(product.code)}
+                      className="gbt ml"
+                    >
+                      delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
           </Table>
         </div>
       </div>
     </>
   );
 };
-const readProduto = async () => {
-  const response = await fetch("http://localhost/routes/products.php");
-  const data = await response.json();
-  return data;
-};
-const clearTable = () => {
-  const rows = document.querySelectorAll("#tbProduto>tbody tr");
-  rows.forEach((row) => row.parentNode.removeChild(row));
-};
-const updateTable = async () => {
-  clearTable();
-  const dbProduto = await readProduto();
-  dbProduto.forEach((product, index) => {
-    createRow(product, index);
-  });
-};
-updateTable();
-const createRow = (product, index) => {
-  const newRow = document.createElement("tr");
-  newRow.innerHTML = `
-      <td>00${product.code}</td>
-      <td>${product.name}</td>
-      <td>${product.amount}</td>
-      <td>$${product.price}</td>
-      <td>${product.category}</td>
-      <td class="tdbutton">
-        <button type="button" onclick="deleteProduto(${product.code})">Delete</button>
-      </td>
-
-    `;
-  document.querySelector("#tbProduto>tbody").appendChild(newRow);
-};
 
 export default Products;
+
+// import Button from "react-bootstrap/Button";
+// import Form from "react-bootstrap/Form";
+// import Table from "react-bootstrap/Table";
+
+// const Products = () => {
+//   return (
+//     <>
+//       <div className="conteiner col-12">
+//         <div className="col-5">
+
+//         </div>
+//         <div className="col-5">
+//           <Table responsive="sm" id="tbProduto">
+//             <thead>
+//               <tr>
+//                 <th>Code</th>
+//                 <th>Product</th>
+//                 <th>Amount</th>
+//                 <th>Unit Price</th>
+//                 <th>products</th>
+//                 <th></th>
+//               </tr>
+//             </thead>
+//             <tbody></tbody>
+//           </Table>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// const readProduto = async () => {
+//   const response = await fetch("http://localhost/routes/products.php");
+//   const data = await response.json();
+//   return data;
+// };
+// const clearTable = () => {
+//   const rows = document.querySelectorAll("#tbProduto>tbody tr");
+//   rows.forEach((row) => row.parentNode.removeChild(row));
+// };
+// const updateTable = async () => {
+//   clearTable();
+//   const dbProduto = await readProduto();
+//   dbProduto.forEach((product, index) => {
+//     createRow(product, index);
+//   });
+// };
+// updateTable();
+// const createRow = (product, index) => {
+//   const newRow = document.createElement("tr");
+//   newRow.innerHTML = `
+//       <td>00${product.code}</td>
+//       <td>${product.name}</td>
+//       <td>${product.amount}</td>
+//       <td>$${product.price}</td>
+//       <td>${product.products}</td>
+//       <td class="tdbutton">
+//         <button type="button" onclick="deleteProduto(${product.code})">Delete</button>
+//       </td>
+
+//     `;
+//   document.querySelector("#tbProduto>tbody").appendChild(newRow);
+// };
+
+// export default Products;
